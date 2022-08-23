c52eb2d95cc0d8339a9d5185ded6fa8fdacd2ace
Frodelian <91691109+Frodelian@users.noreply.github.com>
2022-08-23 22:33:24 +0300

    Update Hello_world.cpp

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
  
-int main(int argc, char** argv){
- string name; // User name
- std::cin >> name; // Input user name
- std::cout << "Hello world from " << name << std::endl;
-} 
+using namespace std;
+int
+main(int argc, char** argv)
+{
+  string name;
+  cin >> name;
+  cout << "Hello world from " << name << endl;
+}
